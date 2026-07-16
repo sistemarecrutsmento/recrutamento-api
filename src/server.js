d14b400ccac78f3a5dc95add434c1b5245b9b962
sistemarecrutsmento <fabio08dejesusjunior@gmail.com>
2026-07-16 16:20:34 -0300

    fix: voltar para bcrypt nativo + npm rebuild --build-from-source

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 require('dotenv').config();
